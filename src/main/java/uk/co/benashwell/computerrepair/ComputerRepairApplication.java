83de0fd98f860207952ef7cb23c443f269a7bf8a
ben_ashwell <baashwell@gmail.com>
2018-12-18 11:25:04 +0000

    Initial Commit

---
+++ src/main/java/uk/co/benashwell/computerrepair/ComputerRepairApplication.java
@@ -0,0 +1,14 @@
+package uk.co.benashwell.computerrepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerRepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerRepairApplication.class, args);
+	}
+
+}
+
